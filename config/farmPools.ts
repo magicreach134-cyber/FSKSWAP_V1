fa4c0380da940a912f8ce3d4255fabd2491b20d6
magicreach134-cyber <magicreach134@gmail.com>
2026-02-18 17:27:57 +0100

    Create farmPools.ts

---
+++ config/farmPools.ts
@@ -0,0 +1,8 @@
+export const FARM_POOLS = [
+  { pid: 0, pair: "FSK/BTC" },
+  { pid: 1, pair: "FSK/ETH" },
+  { pid: 2, pair: "FSK/SOL" },
+  { pid: 3, pair: "FSK/USDC" },
+  { pid: 4, pair: "FSK/FUSDT" },
+  { pid: 5, pair: "FSK/WBNB" },
+];
