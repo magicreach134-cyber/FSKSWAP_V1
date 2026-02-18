ab301709349592be71d0023866a62a83d0bdce59
magicreach134-cyber <magicreach134@gmail.com>
2026-02-18 17:27:08 +0100

    Create pairs.ts

---
+++ config/pairs.ts
@@ -0,0 +1,26 @@
+export const PAIRS = [
+  {
+    address: "0x3A8eAcF25D83f1Ff6B552875a45C0906C1A707DF",
+    symbol: "FSK/BTC",
+  },
+  {
+    address: "0x6018D4C84Ee027B5A8d95DfD143eB499a6A4B490",
+    symbol: "FSK/ETH",
+  },
+  {
+    address: "0xA1F247D5c6F6b40053B12A6E743b276Ffb6C4077",
+    symbol: "FSK/SOL",
+  },
+  {
+    address: "0xEd92DacEF029a02FAad636a5A14aBbB36eB54962",
+    symbol: "FSK/USDC",
+  },
+  {
+    address: "0xCf0A5F2A8F89491B3A17430D9A4770911368D5F0",
+    symbol: "FSK/FUSDT",
+  },
+  {
+    address: "0x3e97F9904eB06FF8595825d7770E760A6581222F",
+    symbol: "FSK/WBNB",
+  },
+];
