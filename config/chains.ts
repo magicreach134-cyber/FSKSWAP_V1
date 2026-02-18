eead72406ab32e6c761c27841f52dd70077af27a
magicreach134-cyber <magicreach134@gmail.com>
2026-02-18 17:14:26 +0100

    Create chains.ts

---
+++ config/chains.ts
@@ -0,0 +1,11 @@
+export const BNB_TESTNET = {
+  id: 97,
+  name: "BNB Smart Chain Testnet",
+  nativeCurrency: {
+    name: "BNB",
+    symbol: "tBNB",
+    decimals: 18,
+  },
+  rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  explorer: "https://testnet.bscscan.com",
+};
