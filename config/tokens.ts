9d6ad00fcdb2a38c028549fbc3256b2b6a8e8773
magicreach134-cyber <magicreach134@gmail.com>
2026-02-18 17:26:26 +0100

    Create tokens.ts

---
+++ config/tokens.ts
@@ -0,0 +1,59 @@
+import { Token } from "@/types";
+
+export const FSK: Token = {
+  address: "0x784f97B0c8116727F8B6417b86975F77411e219B",
+  name: "Fressking",
+  symbol: "FSK",
+  decimals: 18,
+  logoURI: "/tokens/fsk.png",
+};
+
+export const BTC: Token = {
+  address: "0xd397b48d6fAf5311C166aEd21313F48e2a574525",
+  name: "Bitcoin",
+  symbol: "BTC",
+  decimals: 18,
+  logoURI: "/tokens/btc.png",
+};
+
+export const ETH: Token = {
+  address: "0x122CcA6190b40De4fD3EE28A97C995D8C6524921",
+  name: "Ethereum",
+  symbol: "ETH",
+  decimals: 18,
+  logoURI: "/tokens/eth.png",
+};
+
+export const SOL: Token = {
+  address: "0x39c833E1F576D372E221e31cE82Dd52b520e970C",
+  name: "Solana",
+  symbol: "SOL",
+  decimals: 18,
+  logoURI: "/tokens/sol.png",
+};
+
+export const USDC: Token = {
+  address: "0x1c5206Eeb5DF5127204495969955000D15D1c0Be",
+  name: "USD Coin",
+  symbol: "USDC",
+  decimals: 18,
+  logoURI: "/tokens/usdc.png",
+};
+
+export const FUSDT: Token = {
+  address: "0x02F1303f087C6D78F4142bc2dE8430348982d549",
+  name: "FUSDT",
+  symbol: "FUSDT",
+  decimals: 18,
+  logoURI: "/tokens/fusdt.png",
+};
+
+export const WBNB: Token = {
+  address: "0xae13d989dac2f0debff460ac112a837c89baa7cd",
+  name: "Wrapped BNB",
+  symbol: "WBNB",
+  decimals: 18,
+  logoURI: "/tokens/wbnb.png",
+};
+
+export const TOKENS = [FSK, BTC, ETH, SOL, USDC, FUSDT, WBNB];
