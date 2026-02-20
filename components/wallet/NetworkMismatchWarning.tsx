ea722b92a5d305f872d660c8afae89477c76ffc2
magicreach134-cyber <magicreach134@gmail.com>
2026-02-20 13:45:36 +0100

    Create NetworkMismatchWarning.tsx

---
+++ components/wallet/NetworkMismatchWarning.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function NetworkMismatchWarning() {
+  return (
+    <div className="bg-red-500/10 text-red-500 text-sm p-3 rounded-xl">
+      You are connected to the wrong network.
+    </div>
+  );
+}
