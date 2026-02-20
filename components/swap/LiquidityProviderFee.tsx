3d1cc15a10b03e8317e2f883aaf961934cf16070
magicreach134-cyber <magicreach134@gmail.com>
2026-02-20 14:39:53 +0100

    Create LiquidityProviderFee.tsx

---
+++ components/swap/LiquidityProviderFee.tsx
@@ -0,0 +1,8 @@
+export default function LiquidityProviderFee() {
+  return (
+    <div className="flex justify-between">
+      <span>Liquidity Provider Fee</span>
+      <span>0.25%</span>
+    </div>
+  );
+}
