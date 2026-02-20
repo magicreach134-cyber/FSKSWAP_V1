ab704c862108919589da2d7d495ccd139fedbd72
magicreach134-cyber <magicreach134@gmail.com>
2026-02-20 14:39:18 +0100

    Create MinimumReceived.tsx

---
+++ components/swap/MinimumReceived.tsx
@@ -0,0 +1,8 @@
+export default function MinimumReceived() {
+  return (
+    <div className="flex justify-between">
+      <span>Minimum Received</span>
+      <span>0.00</span>
+    </div>
+  );
+}
