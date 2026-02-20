498ab8582fb384e78ea264a9e1226b24a8a6ed15
magicreach134-cyber <magicreach134@gmail.com>
2026-02-20 14:38:07 +0100

    Create PriceImpact.tsx

---
+++ components/swap/PriceImpact.tsx
@@ -0,0 +1,8 @@
+export default function PriceImpact() {
+  return (
+    <div className="flex justify-between">
+      <span>Price Impact</span>
+      <span className="text-yellow-400">0.12%</span>
+    </div>
+  );
+}
