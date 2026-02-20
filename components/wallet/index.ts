da39c4c9e277646c8bd82267c9b3720e9d58c67e
magicreach134-cyber <magicreach134@gmail.com>
2026-02-20 13:46:14 +0100

    Create index.ts

---
+++ components/wallet/index.ts
@@ -0,0 +1,8 @@
+export { default as WalletModal } from "./WalletModal";
+export { default as ConnectOptions } from "./ConnectOptions";
+export { default as AccountDetails } from "./AccountDetails";
+export { default as CopyAddressButton } from "./CopyAddressButton";
+export { default as ExplorerLink } from "./ExplorerLink";
+export { default as DisconnectButton } from "./DisconnectButton";
+export { default as UnsupportedNetwork } from "./UnsupportedNetwork";
+export { default as NetworkMismatchWarning } from "./NetworkMismatchWarning";
