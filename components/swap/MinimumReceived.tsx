b81d827fb872fbc8581aa5fec048e21129560821
magicreach134-cyber <magicreach134@gmail.com>
2026-02-21 09:25:32 +0100

    Update MinimumReceived.tsx

--- components/swap/MinimumReceived.tsx
+++ components/swap/MinimumReceived.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { useSwapStore } from "@/store/swapStore";
+import { calculateMinOut } from "@/utils/calculateMinOut";
+import { parseUnits, formatUnits } from "viem";
+
 export default function MinimumReceived() {
+  const { toAmount, slippage } = useSwapStore();
+
+  if (!toAmount) return null;
+
+  const min = calculateMinOut({
+    quotedAmountOut: toAmount,
+    slippage,
+    decimals: 18,
+  });
+
   return (
     <div className="flex justify-between">
       <span>Minimum Received</span>
-      <span>0.00</span>
+      <span>
+        {formatUnits(min, 18)}
+      </span>
     </div>
   );
 }
